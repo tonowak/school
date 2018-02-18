335a215a2335a2143476c3181c0f1d3919977e47
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-02-18 22:28:57 +0100

    Made update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+version=0
 
+echo Cloning updates from github
+cd ../
+rm -rf school
+git clone https://github.com/tonowak/school > /dev/null
+cd school
+chmod -R 771 updates
+echo
+
+for ((i=version + 1; ; ++i))
+do
+	if [ -e updates/$i.sh ]
+	then
+		echo Update number $i:
+		sh updates/$i.sh
+		sed -i "2s/.*/version=$i/" update.sh
+		echo
+	else
+		echo Made all updates, you can close the terminal
+		exit 0
+	fi
+done
