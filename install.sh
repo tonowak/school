921536292f912f21311538813505858e8ccefd5c
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-06-03 23:44:46 +0200

    wrote update script

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ apt-get -y update
 apt-get -y install git
 
 cd /opt/
-# rm -rf school/
-# git clone https://github.com/tonowak/school
+rm -rf school/
+git clone https://github.com/tonowak/school
 
 ln -s /opt/school/update.sh /etc/init.d/update.sh
 chmod 755 /etc/init.d/update.sh
