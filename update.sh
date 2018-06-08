b4abb5b7a617738867916202a854a0c766611772
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-06-08 11:55:50 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+version=0
+
 ### BEGIN INIT INFO
 # Provides:          school_update
 # Required-Start:    networking
@@ -11,49 +14,58 @@
 #                    13 LO Szczecin
 ### END INIT INFO
 
-#!/bin/bash
-version=0
+start() {
+	LOG="/opt/school_log"
 
-echo "STARTING SCRIPT $(date)" >>/opt/school_log 2>&1
+	echo "STARTING SCRIPT $(date)" >>$LOG 2>&1
 
-echo "WAITING FOR NETWORK" >>/opt/school_log 2>&1
-while ! ping -c 5 google.com > /dev/null
-do
-	echo "No internet connection, waiting 5 seconds" >>/opt/school_log 2>&1
-	sleep 5
-done
+	echo "WAITING FOR NETWORK" >>$LOG 2>&1
+	while ! ping -c 5 google.com > /dev/null
+	do
+		echo "No internet connection, waiting 5 seconds" >>$LOG 2>&1
+		sleep 5
+	done
 
-echo "Found internet connection!" >>/opt/school_log 2>&1
-echo "CLONING FROM GIT" >>/opt/school_log 2>&1
+	echo "Found internet connection!" >>$LOG 2>&1
+	echo "CLONING FROM GIT" >>$LOG 2>&1
 
-cd /opt/
-rm -rf school >>/opt/school_log 2>&1
-git clone https://github.com/tonowak/school >>/opt/school_log 2>&1
-cd /opt/school/ >>/opt/school_log 2>&1
-chmod -R 771 updates >>/opt/school_log 2>&1
-echo >>/opt/school_log 2>&1
+	cd /opt/
+	rm -rf school >>$LOG 2>&1
+	git clone https://github.com/tonowak/school >>$LOG 2>&1
+	cd /opt/school/ >>$LOG 2>&1
+	chmod -R 771 updates >>$LOG 2>&1
+	echo >>$LOG 2>&1
 
-sed -i "2s/.*/version=$i/" update.sh >>/opt/school_log 2>&1
-for ((i=version + 1; ; ++i))
-do
-	if [ -e updates/$i.sh ]
+	sed -i "2s/.*/version=$i/" update.sh >>$LOG 2>&1
+	for ((i=version + 1; ; ++i))
+	do
+		if [ -e updates/$i.sh ]
+		then
+			echo "UPDATE NUMBER $i:" >>$LOG 2>&1
+			updates/$i.sh >>$LOG 2>&1
+			sed -i "2s/.*/version=$i/" update.sh >>$LOG 2>&1
+			echo >>$LOG 2>&1
+		else
+			break;
+		fi
+	done
+
+	if ! diff -r /opt/school/guest-default /home/guest-default > /dev/null
 	then
-		echo "UPDATE NUMBER $i:" >>/opt/school_log 2>&1
-		updates/$i.sh >>/opt/school_log 2>&1
-		sed -i "2s/.*/version=$i/" update.sh >>/opt/school_log 2>&1
-		echo >>/opt/school_log 2>&1
-	else
-		break;
+		echo "COPYING GUEST" >>$LOG 2>&1
+		rm -r /home/guest-default/ >>$LOG 2>&1
+		mkdir /home/guest-default/ >>$LOG 2>&1
+		cp -r /opt/school/guest-default/ /home/ >>$LOG 2>&1
+		chown guest-default /home/guest-default -R >>$LOG 2>&1
 	fi
-done
 
-if ! diff -r /opt/school/guest-default /home/guest-default > /dev/null
-then
-	echo "COPYING GUEST" >>/opt/school_log 2>&1
-	rm -r /home/guest-default/ >>/opt/school_log 2>&1
-	mkdir /home/guest-default/ >>/opt/school_log 2>&1
-	cp -r /opt/school/guest-default/ /home/ >>/opt/school_log 2>&1
-	chown guest-default /home/guest-default -R >>/opt/school_log 2>&1
-fi
+	echo "ENDED UPDATE AT $(date)" >>$LOG 2>&1
+}
 
-echo "ENDED UPDATE AT $(date)" >>/opt/school_log 2>&1
+case "$1" in
+  start)
+    start
+    ;;
+  *)
+	  echo "Usage: update.sh start"
+esac
