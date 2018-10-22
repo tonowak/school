775ac13e5977f6117e51b1a2c657640f71f33225
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-10-22 08:51:07 +0200

    Create 5.sh

---
+++ updates/5.sh
@@ -0,0 +1,7 @@
+# installing zsh
+
+sudo apt-get install zsh -y
+mkdir /home/guest-default/.oh-my-zsh/
+export ZSH="/home/guest-default/.oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i 's/^DSHELL=.*/DSHELL=\/bin\/zsh/g' /etc/adduser.conf
