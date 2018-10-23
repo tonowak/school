d8522e04b8806d8509c739a19b8cfc0052a67a80
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2018-10-23 16:08:06 +0200

    failed update #5

--- updates/5.sh
+++ updates/5.sh
@@ -1,8 +1,3 @@
-# installing zsh
+# failed update
 
 sudo apt-get update
-sudo apt-get install zsh -y
-mkdir /home/guest-default/.oh-my-zsh/
-export ZSH="/home/guest-default/.oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sed -i 's/^DSHELL=.*/DSHELL=\/bin\/zsh/g' /etc/adduser.conf
