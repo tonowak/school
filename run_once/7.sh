92884ec30dcdb337629f4078f819def434ac7e7e
Tomasz Nowak <36604952+tonowak@users.noreply.github.com>
2020-01-11 13:06:17 +0100

    Update 7.sh

--- run_once/7.sh
+++ run_once/7.sh
@@ -1,3 +1,3 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install gcc-9 g++-9 g++-9-multilib clang-6.0 fpc
+sudo apt-get update -y
+sudo apt-get install gcc-9 g++-9 g++-9-multilib clang-6.0 fpc -y
